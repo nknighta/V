7fb1f80e7fc9fdc0208abf242b9ee4c3a172f7b0
NknightA <n.knight.pc0627@gmail.com>
2023-04-26 17:45:56 +0900

    fix db bug

--- memo.md
+++
@@ -1,7 +0,0 @@
-```js
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-
-```
-
-http://localhost:4100/?id=111&page=aa
---
+++ src/components/prismaClient.tsx
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient = new PrismaClient();
+
+export default function PrismaClientElement () {
+
+    return(
+        <>
+
+        </>
+    )
+};
